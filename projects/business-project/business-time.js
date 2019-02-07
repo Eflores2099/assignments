acebef12fdc6614674af237ac01cc1f48f928365
Erik Flores <erik.flores@colorado.edu>
2019-02-06 18:00:11 -0700

    peanut butter

--- projects/business-project/business-time.js
+++ projects/business-project/business-time.js
@@ -1,8 +1,24 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+var isNavBarShowing = false
+var myNavBar = document.getElementById("myTopNav")
+var myNavContainer = document.getElementsByClassName("nav-bar-container")[0];
+var myButton = document.getElementsByClassName("menu-button")[0];
+var closeButton = document.getElementById("close-button");
+
+
+function toggleNavBar() {
+    isNavBarShowing = true 
+    myNavBar.className = "topnav-show"
+    myButton.style.display = "none";
+    myNavContainer.style.display = "block"
+}
+
+closeButton.addEventListener("click", function () {
+    isNavBarShowing = false;
+    console.log(isNavBarShowing)
+    myNavBar.className = "topnav-hidden"
+    setTimeout(function () {
+        myNavContainer.style.display = "none"
+    }, 1001)
+    myButton.style.display = "block";
+}
+)
