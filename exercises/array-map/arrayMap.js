b06c9ccc00e6e892ae00a3c2f2e42600f1947172
Erik Flores <erik.flores@colorado.edu>
2019-02-08 17:00:37 -0700

    peanut butter

---
+++ exercises/array-map/arrayMap.js
@@ -0,0 +1,15 @@
+// Array Map Exercises
+// 27 SEPTEMBER 2017
+// Use the built-in .map() method on arrays to solve all of these problems
+
+// Feel free to copy and paste the function and tests in this assignment.
+
+// 1) Make an array of numbers that are doubles of the first array.
+
+function doubleNumbers(arr){
+arr.map(function(arr){
+    
+})
+    return arr[i] *2 
+}  
+  console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
--- exercises/escape-room/escapeRoom.js
+++ exercises/escape-room/escapeRoom.js
@@ -2,24 +2,31 @@ var ask = require("readline-sync");
 
 var name = ask.question("What is you name?");
 
-console.log ("standard greeting phrase "+ name);
+console.log ("Hello, and welcome to my game! "+ name);
 
 var options = ["Put hand in hole", "Find the key", "Open the door"]
 var gameOver = false
 //  Build var for finding the key
-var findKey = 
+var findKey = false
 // game loop
 while(!gameOver){
     
-    ask.keyInSelect(options, "what would you like to do?")
+        var userChoice = ask.keyInSelect(options, 'What would like to do?')
         if(userChoice === 0){
             console.log ("you put your hand in the hole, and a bear ate you.")
             gameOver = true
         }else if(userChoice === 1){
             console.log ("You chose to find the key. Now open the door?")
-            gameOver = false
+            findKey = true 
         }else if( userChoice === 2){
-            console.log("you need a key to open the door.")
-            gameOver = false
-        }else ( userChoice == 2)
+            console.log("you need a key to open the door, dum dum.")
+            if(findKey ===true){
+                console.log(" You have escaped!!")
+                gameOver=true
+            }else if(findKey===false){
+                console.log("you dont have a key,stupid. You can't leave.")
+            }
+        }else if( userChoice === -1){
+            gameOver =true
+        }
 }
