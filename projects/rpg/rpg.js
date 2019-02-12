fde8d10b8579ffa1837848e520df2b11cf8653f3
Erik Flores <erik.flores@colorado.edu>
2019-02-11 19:00:49 -0700

    updated rpg

--- projects/rpg/rpg.js
+++ projects/rpg/rpg.js
@@ -6,39 +6,45 @@ var ask = require('readline-sync')
 var player = {
     name: '',
     health: 100,
-    inventory: []
+    inventory: [],
+    attackDamage: 50
 }
 
 var enemy = [
     {
         name: 'Monarch Henchman',
         health: 50,
-        inventory: ["Dart Gun", "Xanax"]
+        inventory: ["Dart Gun", "Xanax"],
+        attackDamge: 30,
     },
     {
         name: "The Monarch",
         health: 75,
-        inventory: ["Monarch Suit", "Xanax"]
+        inventory: ["Monarch Suit", "Xanax"],
+        attackDamage: 35,
     },
     {
         name: "Baron Underbheit",
         health: 105,
-        inventory: ["Underbheit's Lower Jaw", "Xanax"]
+        inventory: ["Underbheit's Lower Jaw", "Xanax"],
+        attackDamage: 35,
     },
     {
         name: "Henry Killinger",
         health: 95,
-        inventory: ["Killinger's Umbrella", "Xanax"]
+        inventory: ["Killinger's Umbrella", "Xanax"],
+        attackDamage: 35,
     },
     {
         name: "Molotov Cocktease",
         health: 120,
-        inventory: ["Chastity Belt", "Xanax"]
+        inventory: ["Chastity Belt", "Xanax"],
+        attackDamage: 35,
     }]
 
 var playerOption = ["Walk.", "Check Inventory"]
 
-
+var attackOrRun = ["Attack", "Run"]
 
 // /////////////////
 // /// GAME INTRO //
@@ -51,10 +57,12 @@ console.log("Welcome " + player.name + ", to The Venture Bros. Game")
 function walk() {
     console.log("press 1 to walk.")
     var chance = Math.floor(Math.random() * 4)
-    if (chance === Math.floor(Math.random() * 4)) {
+    console.log(chance)
+    if (chance === 2) {
+        console.log("im running")
         fight();
     } else {
-        console.log("You have such and such in your bag and this many health points. push 1 to walk.")
+        console.log("You have such and such in your bag and this many health points.")
     }
 
 }
@@ -68,26 +76,57 @@ function run() {
     }
 }
 
-function enemyCreation(){
+function enemyCreation() {
     var randomEnemy = Math.floor(Math.random() * enemy.length)
     return enemy[randomEnemy]
 }
 
 function fight() {
-    console.log("Do you want to fight or run?")
     var newEnemy = enemyCreation()
     console.log(newEnemy)
-    while (enemy.health > 0 && player.health > 0)
-        if (playerOption === 3) {
-            run()
-        } else {
-            attackEnemy()
+    console.log("Do you want to fight or run?")
+    var playerChoice = ask.keyInSelect(attackOrRun, "What would you like to do?")
+    if (playerChoice === 0) {
+        while (newEnemy.health > 0 && player.health > 0) {
+            var newplayerChoice = ask.keyInSelect(attackOrRun, "What would you like to do?")
+            if (newplayerChoice === 0) {
+                console.log("attack")
+                newEnemy.health -= player.attackDamage
+                if (newEnemy.health <= 0) {
+                    console.log("Enemy died")
+                    const currentEnemy = enemy.findIndex(each => each.name === newEnemy.name)
+                    enemy.splice(currentEnemy, 1);
+                    return walk()
+                } else {
+                    player.health -= newEnemy.attackDamage
+                }
+                console.log(newEnemy.health)
+                console.log(player.health)
+            } else if (newplayerChoice === 1) {
+                return run()
+            }
         }
+    }
+    else{
+        return run()
+    }
 }
+
 // function attackEnemy(){
-//    var attackDamge = Math.floor(Math.random() * 4) 
+//     var attackDamge = Math.floor(Math.random() * 25) 
+//     enemy.health -= attackDamge
+
+//     enemyAttack()
+
 
 // }
+// Function enemyAttack(){
+//     var attackDamage = Math.floor(Math.random()) *25)
+//     player.health -= attackDamge
+// }
+
+
+
 
 
 
