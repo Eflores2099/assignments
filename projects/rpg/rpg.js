e726d831bb203152451d343aa28abba42a6e10b3
Erik Flores <erik.flores@colorado.edu>
2019-02-11 17:38:41 -0700

    peanut butter

--- projects/rpg/rpg.js
+++ projects/rpg/rpg.js
@@ -8,45 +8,35 @@ var player = {
     health: 100,
     inventory: []
 }
-var playerOption = ["w","a","r"]
-var enemies =['Monarch Henchman', "The Monarch", " Phantom Limb", "Baron Underbheit","Henry Killinger, Molotov Cocktease"]
-var options = ["Walk. Press W ", "Attack. Press A", "Run Away. Press R."]
-var userChoice= ask.question("Do something," + player.name + "!")
-var gameOver = false
-////////////////////
-// Game Functions //
-function walk(){
-    var chance = Math.floor(Math.random() * 4)
-    if(playerOption ==="w")
-        if(chance = Math.floor(Math.random() * 4)){
-            fight();
-        }else {
-            // console.log( You have such and such in your bag and this many health points. push w to walk.)
-        }
-}
-function run(){
-    if(playerOption === "r"){
-        var chance = Math.floor(Math.random()* 2)
-        if(chance === 1){
-            console.log("You successfuly ran away, wuss!")
-        }
-    }else{
-        console.log("Double damn it! Play him some of your chin music!")
-            attack()
-    }
-}
-function fight(){
-    console.log("Do you want to fight or run?")
-    enemyCreation()
-    if(playerOption === "r"){
-        run()
-    }else{
-        attackEnemy()
-    }
-}
 
+var enemy = [
+    {
+        name: 'Monarch Henchman',
+        health: 50,
+        inventory: ["Dart Gun", "Xanax"]
+    },
+    {
+        name: "The Monarch",
+        health: 75,
+        inventory: ["Monarch Suit", "Xanax"]
+    },
+    {
+        name: "Baron Underbheit",
+        health: 105,
+        inventory: ["Underbheit's Lower Jaw", "Xanax"]
+    },
+    {
+        name: "Henry Killinger",
+        health: 95,
+        inventory: ["Killinger's Umbrella", "Xanax"]
+    },
+    {
+        name: "Molotov Cocktease",
+        health: 120,
+        inventory: ["Chastity Belt", "Xanax"]
+    }]
 
-
+var playerOption = ["Walk.", "Check Inventory"]
 
 
 
@@ -56,9 +46,57 @@ function fight(){
 player.name = ask.question("What is your name? ");
 console.log("Welcome " + player.name + ", to The Venture Bros. Game")
 
+////////////////////
+// Game Functions //
+function walk() {
+    console.log("press 1 to walk.")
+    var chance = Math.floor(Math.random() * 4)
+    if (chance === Math.floor(Math.random() * 4)) {
+        fight();
+    } else {
+        console.log("You have such and such in your bag and this many health points. push 1 to walk.")
+    }
+
+}
+function run() {
+    var chance = Math.floor(Math.random() * 2)
+    if (chance === 1) {
+        console.log("You ran away successfully, wuss!")
+    } else {
+        console.log("Double damn it! Play him some of your chin music!")
+        attackEnemy()
+    }
+}
+
+function enemyCreation(){
+    var randomEnemy = Math.floor(Math.random() * enemy.length)
+    return enemy[randomEnemy]
+}
+
+function fight() {
+    console.log("Do you want to fight or run?")
+    var newEnemy = enemyCreation()
+    console.log(newEnemy)
+    while (enemy.health > 0 && player.health > 0)
+        if (playerOption === 3) {
+            run()
+        } else {
+            attackEnemy()
+        }
+}
+// function attackEnemy(){
+//    var attackDamge = Math.floor(Math.random() * 4) 
+
+// }
+
+
 
 /////////////
 // GAME LOOP
-while(player.health > 0){
-    walk();
+while (player.health > 0) {
+    var playerChoice = ask.keyInSelect(playerOption, "What would you like to do?")
+    if (playerChoice === 0) {
+        console.log("You explored the compound")
+        walk();
+    }
 }
