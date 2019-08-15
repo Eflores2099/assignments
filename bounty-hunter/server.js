ffe9dabdc4591eb8e895b8ebac558f884c4f5140
Erik Flores <erik.flores@colorado.edu>
2019-08-14 23:04:37 -0600

    update

--- bounty-hunter/server.js
+++ bounty-hunter/server.js
@@ -1,34 +1,3 @@
-// const express =require('express')
-// const app = express()
-// const morgan = require('morgan')
-// const mongoose = require('mongoose')
-
-
-// app.use(express.json())
-// app.use(morgan("dev"))
-
-// mongoose.connect("mongodb:/localhost:7000/bounty", {useNewUrlParser: true}, ()=> {
-//     console.log("Connected to the Data Base")
-// })
-
-
-
-// app.use("/bounty", require('./routes/bountyRoutes.js'))
-
-// app.use((err, req, res, next) => {
-//     console.error(err)
-//     return res.send({errMsg: err.message})
-// })
-
-
-
-
-
-
-// app.listen(7000, () => {
-//     console.log("Server is running on Port 7000")
-// })
-
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
