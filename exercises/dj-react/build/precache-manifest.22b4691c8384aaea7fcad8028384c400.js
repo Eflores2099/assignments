2fbe2a5774ede30b3dc126b943e93c7939518b4d
Erik Flores <erik.flores@colorado.edu>
2019-08-15 23:21:56 -0600

    update

---
+++ exercises/dj-react/build/precache-manifest.22b4691c8384aaea7fcad8028384c400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43aa3f112c7ef19348d9726abe91e2c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a37c4a0d735cc2c24f66",
+    "url": "/static/css/main.3ba77e4c.chunk.css"
+  },
+  {
+    "revision": "232d9e90b561a32ae45f",
+    "url": "/static/js/2.d5a9fc46.chunk.js"
+  },
+  {
+    "revision": "a37c4a0d735cc2c24f66",
+    "url": "/static/js/main.8666865e.chunk.js"
+  },
+  {
+    "revision": "ead736ff4f38b7fdca7c",
+    "url": "/static/js/runtime~main.03ecbf35.js"
+  }
+]);
